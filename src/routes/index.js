eb2c41b33798316dee6b4c9c2d8d92f1ffbae7c6
bimaap <let.bimaap@gmail.com>
2022-09-30 00:24:00 +0700

    fix: cors 2.0

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,13 @@
 const router = require('express').Router();
 
+router.get("/", (req, res) => {
+  res.setHeader("Access-Control-Allow-Origin", "*")
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader("Access-Control-Max-Age", "1800");
+  res.setHeader("Access-Control-Allow-Headers", "content-type");
+  res.setHeader( "Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, PATCH, OPTIONS" ); 
+});
+
 // router.use('/', (req, res)=>res.send("Hallo dari backend"))
 router.use("/contact", require("./contact-us"));
 
