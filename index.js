45c1b49460173ab1d3b8095ed85bb122c214f2a2
bimaap <let.bimaap@gmail.com>
2022-09-30 00:15:28 +0700

    fix; cors

--- index.js
+++ index.js
@@ -4,12 +4,9 @@ const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 8000;
 
-app.use(cors())
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  next();
-});
+app.use(cors({
+  origin: "http://localhost:3000"
+}))
 
 app.unsubscribe(express.json());
 app.use(express.urlencoded({ extended: false }));
