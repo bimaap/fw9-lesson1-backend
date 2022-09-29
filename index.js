5c776ad3eaa9b0f1d4c678091f06332c7cf80341
bimaap <let.bimaap@gmail.com>
2022-09-29 23:51:53 +0700

    fix: cors

--- index.js
+++ index.js
@@ -2,11 +2,12 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
+const port = process.env.PORT || 8000;
 
-app.use(express.urlencoded({ extended: false }));
-app.use(cors());
+app.use(cors())
+app.unsubscribe(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.use("/", require("./src/routes"));
+app.use('/',require('./src/routes/'));
 
-const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
+app.listen(port, () => console.log(`App listening on port ${port}`));
