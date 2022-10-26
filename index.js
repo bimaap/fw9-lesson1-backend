0b8dde137aa867b101e776a497f7cf851b5cab2b
bimaap <let.bimaap@gmail.com>
2022-10-26 11:01:16 +0700

    fix: cors

--- index.js
+++ index.js
@@ -5,8 +5,12 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 8000;
 
-app.use(express.urlencoded({ extended: false }));
-app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors({
+  allowedHeaders: "*",
+  allowMethods: "*",
+  origin: "*"
+}))
 
 app.use('/',require('./src/routes/'));
 
